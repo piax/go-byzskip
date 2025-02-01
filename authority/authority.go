6d243387da31fb9ca16cccd37d98c11048e0ccb9
piax <teranisi@nict.go.jp>
2025-02-01 16:12:34 +0900

    Added file authentication.

--- authority/authority.go
+++ authority/authority.go
@@ -1,14 +1,39 @@
 package authority
 
 import (
+	"os"
+
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/piax/go-byzskip/ayame"
 )
 
+const (
+	AUTH_PUBKEY = "AUTH_PUBKEY"
+)
+
 type Authorizer struct {
 	pubKey  crypto.PubKey
 	privKey crypto.PrivKey
+}
+
+// Global variable to store public key of authority.
+var AuthPubKey crypto.PubKey = nil
+
+func UpdateAuthPubKey() {
+	// since this function is called at bootstrap phase, initialize the public key.
+	if AuthPubKey == nil {
+		pk := os.Getenv(AUTH_PUBKEY)
+		if pk == "" {
+			panic("AUTH_PUBKEY is empty")
+		} else {
+			p, err := UnmarshalStringToPubKey(pk)
+			if err != nil {
+				panic(err)
+			}
+			AuthPubKey = p
+		}
+	}
 }
 
 func NewAuthorizer() *Authorizer {
@@ -43,8 +68,8 @@ func marshalJoinInfo(id peer.ID, key ayame.Key, mv *ayame.MembershipVector) []by
 // Create a new node with its implemented protocols
 func VerifyJoinCert(id peer.ID, key ayame.Key, mv *ayame.MembershipVector, cert []byte, pubKey crypto.PubKey) bool {
 	data := marshalJoinInfo(id, key, mv)
-	//ayame.Log.Debugf("joincert id=%s, key=%s, mv=%s, data=%v", id, key, mv, data)
-	//mHashBuf, _ := multihash.EncodeName(data, "sha2-256")
+	ayame.Log.Debugf("verifying joincert id=%s, key=%s, mv=%s, data=%v, cert=%x", id, key, mv, data, cert)
+
 	res, err := pubKey.Verify(data, cert)
 	if err != nil {
 		ayame.Log.Errorf("Verify error: %s\n", err)
