ce5a057030ca2d92360e12fc012c9b2cefc955b8
piax <teranisi@nict.go.jp>
2025-02-01 16:14:20 +0900

    Add file authentication.

--- authority/webauth.go
+++ authority/webauth.go
@@ -7,41 +7,25 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/piax/go-byzskip/ayame"
 )
 
 const (
-	WEBAUTH_URL    = "WEBAUTH_URL"
-	WEBAUTH_PUBKEY = "WEBAUTH_PUBKEY"
+	WEBAUTH_URL = "WEBAUTH_URL"
 )
-
-var AuthPubKey crypto.PubKey = nil
 
 func WebAuthAuthorize(id peer.ID, key ayame.Key) (ayame.Key, *ayame.MembershipVector, []byte, error) {
 	c, err := authWeb(os.Getenv(WEBAUTH_URL), id, key)
 	if err != nil {
 		return nil, nil, nil, err
 	}
-
-	// since this function is called at bootstrap phase, initialize the public key.
-	if AuthPubKey == nil {
-		pk := os.Getenv(WEBAUTH_PUBKEY)
-		if pk == "" {
-			panic("WEBAUTH_PUBKEY is empty")
-		} else {
-			p, err := UnmarshalStringToPubKey(pk)
-			if err != nil {
-				panic(err)
-			}
-			AuthPubKey = p
-		}
-	}
+	UpdateAuthPubKey()
 	return c.Key, c.Mv, c.Cert, nil
 }
 
-func WebAuthValidate(id peer.ID, key ayame.Key, mv *ayame.MembershipVector, cert []byte) bool {
+func AuthValidate(id peer.ID, key ayame.Key, mv *ayame.MembershipVector, cert []byte) bool {
+	UpdateAuthPubKey()
 	return VerifyJoinCert(id, key, mv, cert, AuthPubKey)
 }
 
@@ -52,7 +36,7 @@ func authWeb(url string, id peer.ID, key ayame.Key) (*PCert, error) {
 	} else {
 		keyStr = MarshalKeyToString(key)
 	}
-	resp, err := http.Get(url + fmt.Sprintf("/issue?key=%s&id=%s", keyStr, id.Pretty()))
+	resp, err := http.Get(url + fmt.Sprintf("/issue?key=%s&id=%s", keyStr, id.String()))
 	if err != nil {
 		return nil, fmt.Errorf("authority error: %s", err)
 	}
