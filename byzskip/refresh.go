f962897839a6ffaae3722d71c86b646b1e20e703
piax <teranisi@nict.go.jp>
2025-07-13 16:44:43 +0900

    Implement FixLowPeers

--- byzskip/refresh.go
+++ byzskip/refresh.go
@@ -5,17 +5,19 @@ import (
 	"sync"
 	"time"
 
-	"github.com/jbenet/goprocess"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/piax/go-byzskip/ayame"
 	p2p "github.com/piax/go-byzskip/ayame/p2p"
+	"go.uber.org/fx"
 )
 
-var periodicBootstrapInterval = 1 * time.Minute
-var RefreshInterval = 2 * time.Minute
-var PingInterval = 1 * time.Minute
+// var periodicBootstrapInterval = 1 * time.Minute // bootstrap the routing table
+var refreshInterval = 20 * time.Minute // refresh the routing table
+//var pingInterval = 1 * time.Minute    // ping all neighbors
 
+/*
 func (n *BSNode) fixLowPeersRoutine(proc goprocess.Process) {
+	ayame.Log.Debugf("%s: fixLowPeersRoutine starting", n)
 	ticker := time.NewTicker(periodicBootstrapInterval)
 	defer ticker.Stop()
 
@@ -24,13 +26,13 @@ func (n *BSNode) fixLowPeersRoutine(proc goprocess.Process) {
 		//		case <-n.fixLowPeersChan:
 		case <-ticker.C:
 		case <-proc.Closing():
+			ayame.Log.Debugf("%s: fixLowPeersRoutine closing", n)
 			return
 		}
 		ayame.Log.Debugf("%s: fixing low peers", n)
 		n.fixLowPeers(context.Background())
 	}
-
-}
+}*/
 
 var peerPingTimeout = 10 * time.Second
 
@@ -57,14 +59,13 @@ func (n *BSNode) pingAll(ctx context.Context) {
 
 func (n *BSNode) fixLowPeers(ctx context.Context) {
 	force := false
+	//	if time.Since(n.lastPing) >= pingInterval {
+	ayame.Log.Debugf("%s: ping to all", n)
+	n.pingAll(ctx)
+	//		n.lastPing = time.Now()
+	//	}
 
-	if time.Since(n.lastPing) >= PingInterval {
-		ayame.Log.Debugf("%s: ping to all", n)
-		n.pingAll(ctx)
-		n.lastPing = time.Now()
-	}
-
-	if time.Since(n.lastRefresh) >= RefreshInterval {
+	if time.Since(n.lastRefresh) >= refreshInterval {
 		force = true
 	}
 
@@ -79,4 +80,32 @@ func (n *BSNode) fixLowPeers(ctx context.Context) {
 		closest:    ksToNs(clst),
 		candidates: []*BSNode{}, queried: []*BSNode{}, failed: []*BSNode{}}
 	n.RefreshRTWait(ctx)
+}
+
+// LowPeersFixer is a function to be used with fx.Invoke
+// The interval parameter specifies the execution interval
+func LowPeersFixer(interval time.Duration) func(lc fx.Lifecycle, node *BSNode) error {
+	return func(lc fx.Lifecycle, node *BSNode) error {
+		lc.Append(fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				go func() {
+					ticker := time.NewTicker(interval)
+					defer ticker.Stop()
+
+					for {
+						select {
+						case <-ticker.C:
+							ayame.Log.Debugf("%s: fixing low peers", node)
+							node.fixLowPeers(context.Background())
+						case <-ctx.Done():
+							ayame.Log.Debugf("%s: fixLowPeersRoutine closing", node)
+							return
+						}
+					}
+				}()
+				return nil
+			},
+		})
+		return nil
+	}
 }
